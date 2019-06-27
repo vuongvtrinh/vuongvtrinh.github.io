9496aeaf9dd1d4c2952905ee0569ef660af68a97
trinhvv <vanvuong.trinh@gmail.com>
2019-06-27 10:45:41 +0200

    Commit time: 2019-06-27 10:45:41

--- update_files.py
+++ update_files.py
@@ -1,12 +1,19 @@
-import shutil, os
+import os
+import shutil
 
-def copytree(src, dst, symlinks=False, ignore=None):
-    for item in os.listdir(src):
-        s = os.path.join(src, item)
-        d = os.path.join(dst, item)
-        if os.path.isdir(s):
-            shutil.copytree(s, d, symlinks, ignore)
-        else:
-            shutil.copy2(s, d)
+root_src_dir = '/code/public'
+root_dst_dir = os.getcwd()
 
-copytree('/code/public', os.getcwd())
+for src_dir, dirs, files in os.walk(root_src_dir):
+    dst_dir = src_dir.replace(root_src_dir, root_dst_dir, 1)
+    if not os.path.exists(dst_dir):
+        os.makedirs(dst_dir)
+    for file_ in files:
+        src_file = os.path.join(src_dir, file_)
+        dst_file = os.path.join(dst_dir, file_)
+        if os.path.exists(dst_file):
+            # in case of the src and dst are the same file
+            if os.path.samefile(src_file, dst_file):
+                continue
+            os.remove(dst_file)
+        shutil.move(src_file, dst_dir)
